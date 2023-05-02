7008bba787a18be03fde88198c6084e979838596
thorp30 <114027757+thorp30@users.noreply.github.com>
2023-05-02 21:38:19 +0100

    Brazil update
    Starting the process of undertaking predictor to work for Brazilian league as this has a good range of stats available and runs over summer

---
+++ historic_scraping_football_data_brazil.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  2 21:27:34 2023
+
+@author: tom
+"""
+
+# Need to scrape 2022 season to prepend to current season (and 2021?)
